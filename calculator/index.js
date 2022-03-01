cc9f6fa8181f30c84454a6c68ee73d21569d7f3e
Zulma Rocio <zrmartinezg@gmail.com>
2022-02-28 21:22:03 -0500

    Add clear functionality

--- calculator/index.js
+++ calculator/index.js
@@ -1,9 +1,24 @@
 
 const result = document.getElementById("result")
+let toBecleared = false;
+
 const appendCalculation = (symbol) => {
+ if(toBecleared === true){
+   clear()
+ }
  result.value = result.value + symbol;
 }
 
 const calculate = () => {
-  result.value += "=" + eval(result.value);
+  toBecleared = true;
+  result.value += " = " + eval(result.value);
+}
+
+const clear = () => {
+  result.value = "";
+  toBecleared = false;
+}
+const clearText = () => {
+  result.value = "";
+  toBecleared = false;
 }
