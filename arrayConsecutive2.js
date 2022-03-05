af4b3c18afbe6dbdcf4f984e160d83c3de83cc03
Zulma Rocio <zrmartinezg@gmail.com>
2022-03-04 20:53:57 -0500

    Add challenge

--- arrayConsecutive2.js
+++ arrayConsecutive2.js
@@ -3,3 +3,16 @@
 //  them from smallest to largest so that each statue will be bigger than the previous one exactly by 1. He may 
 //  need some additional statues to
 //  be able to accomplish that. Help him figure out the minimum number of additional statues needed.
+
+function solution(statues) {
+let min = Math.min(...statues);
+let max = Math.max(...statues);
+let result = 0;
+for (let i = min; i <= max; i++){
+  result ++
+}
+return result - statues.length
+}
+
+console.log(solution([6, 2, 3, 8]))
+// 4
