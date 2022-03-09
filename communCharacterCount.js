d7f3fc363a3a2d0aa954cd3e59d958a7f86019ae
Zulma Rocio <zrmartinezg@gmail.com>
2022-03-08 19:23:48 -0500

    Add challenge

--- communCharacterCount.js
+++ communCharacterCount.js
@@ -6,28 +6,6 @@
 // solution(s1, s2) = 3.
 
 // Strings have 3 common characters - 2 "a"s and 1 "c".
-// function solution(s1, s2) {
-//   let count = 0;
-//   let arrS1 = s1.split("").sort()
-//   let arrS2 = s2.split("").sort()
-// console.log(arrS1)
-// console.log(arrS2)
-//   if ( arrS1.length < arrS2.length ){
-//   for(let i= 0; i < arrS1.length; i++){
-//     if(arrS1[i] === arrS2[i]){
-//       count ++
-//     }
-//   } 
-//   } else if (arrS2.length < arrS1.length  || arrS2.length === arrS1.length  ){
-//     for(let i= 0; i < arrS2.length; i++){
-//       if(arrS1[i] === arrS2[i]){
-//         count ++
-//       }
-//     } 
-
-//   }
-// return count
-// }
 
 function solution(s1, s2) {
   let result = 0;
